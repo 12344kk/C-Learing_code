bf26c8ed01a6cf89fa818cefc08091309a5317fa
att <111111@qq.com>
2026-03-29 23:38:48 +0800

    3-29_1

--- code/helloworld.cpp
+++ code/helloworld.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+
 #if 0
 使用if 0 进行注释
 将#if 0 改为 #if 1 就可以将注释取消
@@ -10,7 +12,7 @@
 
 // 比如需要写一个函数我需要使用的时候就打开，不要使用时也可以手动关闭
 #if 1   // 0:注释代码  1:启动代码
-void coutHello()
+    void coutHello()
 {
     std::cout << "hello !"<< std::endl;
 }
@@ -20,8 +22,10 @@ void coutHello()
 
 int main()
 {
+    
     std::cout << "hello world !"<< std::endl;
     coutHello();
+    std::cout << sizeof(int) << std::endl;   // 使用sizeof可以获取数据类型的大小，例如int占4B
     std::cin.get();     //作用：实现点按回车后才结束程序
 }
 
